b245fd71b00ca3b791f99da52d6dc26bbc7ce4ef
Inês Reis <mimireis2002@gmail.com>
2020-12-24 18:14:50 +0000

    24-12 -> interface geral e criação das funções

--- activity-status/Interface+code.py
+++ activity-status/Interface+code.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
+
+# definida a window
 window = Tk()
 window.geometry("1000x750")
 window.title("To-Do List")
 
 
-# definir as funções e atribuir aos respetivos butões
+# definir as funções e atribuir aos respetivos botões
 
-def save():
+def save_changes():
     task = entry_title.get()
     description = txt_description.get()
     state = value.get()
@@ -23,41 +25,36 @@ def edit():
 def delete():
 
 
+def save_comment():
 
 
-
-
-
-
-# Caixa com a lista de tarefas ----------------------------
+# Caixa com a lista de tarefas 
 painel_items = PanedWindow(window, width=350, height=400, bd=2, relief= "sunken")
 painel_items.place( x= 10, y=100) 
-# ---------------------------------------------------------------
 
-# Caixa com botões de editar ou remover -------------------
+
+
+# Caixa com botões de criar, editar ou remover 
 painel_btn = PanedWindow(window, width=350, height=100, bd=2, relief= "sunken")
 painel_btn.place( x= 10, y=525) 
 
-btn_create = Button(painel_btn, text="Criar", width=10)
+btn_create = Button(painel_btn, text="Criar", width=10, command = create)
 btn_create.place(x=30, y=35)
 
-
-btn_edit = Button(painel_btn, text="Editar", width=10)
+btn_edit = Button(painel_btn, text="Editar", width=10, command = edit)
 btn_edit.place(x=130, y=35)
 
-btn_remove = Button(painel_btn, text="Remover", width=10)
+btn_remove = Button(painel_btn, text="Remover", width=10, command = remove)
 btn_remove.place(x=230, y=35)
 
-#--------------------------------------------------------------------
 
-# Caixa com outros botões --------------------------------------------
+
+# Caixa para comentar 
 painel_comment= PanedWindow(window, width=500, height=150, bd=2, relief= "sunken")
 painel_comment.place( x= 450, y=525) 
 
-btn_save = Button(painel_comment, text="Salvar", width=10)
+btn_save = Button(painel_comment, text="Salvar", width=10, command = save_comment)
 btn_save.place(x=10, y=80)
-
-
 
 lbl_comment = Label(painel_comment, text="Comentar:")
 lbl_comment.place(x=20, y = 40)
@@ -71,19 +68,21 @@ txt_comment.place(x=150, y = 30)
 painel_to_do = PanedWindow(window, width=500, height=400, bd=2, relief= "sunken")
 painel_to_do.place( x= 450, y=100) 
 
+#titulo
 lbl_title = Label(painel_to_do, text="Título:")
 lbl_title.place(x=15, y = 30)
 
 entry_title = Entry(painel_to_do, width = 50)
 entry_title.place(x = 90, y= 30)
 
-
+# descrição
 lbl_description = Label(painel_to_do, text="Descrição:")
 lbl_description.place(x=15, y = 70)
 
 txt_description = Text(painel_to_do, width = 55, height = 5)
 txt_description.place(x=15, y = 100)
 
+# se está concluido ou não
 frame = LabelFrame(painel_to_do, width=200, height=150,bd ="3", relief="sunken", fg="blue", text="Estado")
 frame.place (x=15,y=200)
 selected = IntVar()
@@ -97,7 +96,8 @@ radio.place(x=40,y=20)
 radio2.place(x=40, y=50)
 radio3.place(x=40, y=80)
 
-btn_save = Button(painel_to_do, text="Salvar", width=10, command = save)
+# salvar alterações
+btn_save = Button(painel_to_do, text="Salvar", width=10, command = save_changes)
 btn_save.place(x=410, y=360)
 
 
